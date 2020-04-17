d50d8079bf75c68a2073aa57a12fb3ba69fbd5cc
YairCastillo <pabloyair993@gmail.com>
2020-04-16 20:23:06 -0500

    Update EntrevistaInicial 3
    Ya aparece la primera pestaña al cargar la página

--- view/entrevistaInicial.php
+++ view/entrevistaInicial.php
@@ -9,7 +9,7 @@
 <html lang="es">
 <head>
 <title>Entrevista Inicial</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"><script src="js/jquery-3.4.1.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"><script src="../js/jquery-3.4.1.js"></script>
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -20,7 +20,6 @@
 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" type="text/css" href="../css/estilo.css"/>
-	  <script type="text/javascript" src="../js/cambiarPestanna.js"></script>
 </head>
 <body>
     <!-- Tab links -->
@@ -35,6 +34,8 @@
   <button class="tablinks" onclick="openCity(event, 'Salud')">Salud</button>
 </div>
 <br><br>
+
+
 <!-- Tab content -->
 <div id="Personales" class="tabcontent">
         <!--Contenido de la pestaña 1-->
@@ -597,5 +598,6 @@
         </form>
 </div>
 
+<script type="text/javascript" src="../js/cambiarPestanna.js"></script>
 </body>
 </html>
